661b77a2101a74bb270e1ce2db2dcc54c0f82d1f
Kristaps Štrāls <kristaps@krisstech.com>
2018-05-02 10:26:34 +0300

    simpler wedding website

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const app = express();
 
-// API file for interacting with MongoDB
-// const api = require('./server/routes/api');
+app.use(express.static('static'))
 
-// Parsers
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-
-// Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// API location
-// app.use('/api', api);
-
-// Send all other requests to the Angular app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 //Set Port
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '8080';
 app.set('port', port);
 
 const server = http.createServer(app);
---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+$("#link").click(function() {
+   scrollToAnchor('form');
+});
